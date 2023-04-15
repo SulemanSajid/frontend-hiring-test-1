55af6b66b26c45ee3abd69edac8b781b25075daa
Suleman <sulemansajid777@gmail.com>
2023-04-16 02:05:59 +0500

    login page complete

--- turingFrontend-app/src/App.js
+++ turingFrontend-app/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import * as Pages from "./pages";
+import Login from "./components/Login";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function PrivateRoute({ children }) {
+  const loggedInAs = localStorage.getItem("logged-in-as") || null;
+
+  if (!loggedInAs) return <Navigate to="/" />;
+
+  return children;
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Turing Tech</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/Home/*"
+            element={
+              <PrivateRoute>
+                <Pages.Home />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Pages.Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <h1>Turing Tech</h1> */}
     </div>
   );
 }
